51cfa3525bcf1d309feed2d436ce49010dfee939
yd0384 <youngdeok9@naver.com>
2021-07-21 20:59:56 +0900

    0721

---
+++ managefront/src/components/LoginPage.vue
@@ -0,0 +1,64 @@
+<template>
+    <div>
+        <h2>Log In</h2>
+        <b-form @submit="onSubmit">
+            <b-form-group
+                id="input-group-1"
+                label="ID:"
+                label-for="input-1"
+            >
+                <b-form-input
+                    id="input-1"
+                    v-model="form.id"
+                    type="id"
+                    placeholder="Enter id"
+                    required
+                ></b-form-input>
+            </b-form-group>
+            <b-form-group
+                id="input-group-2"
+                label="Password:"
+                label-for="text-password"
+            >
+                <b-form-input
+                    id="text-password"
+                    v-model="form.password"
+                    type="password"
+                    placeholder="Enter password"
+                    required
+                ></b-form-input>
+            </b-form-group>
+            <b-form-group id="input-group-2" label="Level:" label-for="input-2">
+                <b-form-select
+                id="input-2"
+                v-model="form.level"
+                :options="levels"
+                required
+                ></b-form-select>
+            </b-form-group>
+
+            <b-button type="submit" variant="primary">Submit</b-button>
+        </b-form>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            form: {
+                id: '',
+                password: '',
+                level: null
+            },
+            levels: [{text: 'Select One', value: null}, 'Level1', 'Level2', 'Level3'],
+            show: true
+        }
+    },
+    methods: {
+        onSubmit(event) {
+            event.preventDefault()
+            alert(JSON.stringify(this.form))
+        }
+    }
+}
+</script>
--- managefront/src/main.js
+++ managefront/src/main.js
@@ -4,9 +4,12 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import axios from 'axios'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.prototype.$http = axios
-
+Vue.use(BootstrapVue)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
