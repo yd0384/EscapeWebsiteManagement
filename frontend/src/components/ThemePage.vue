649ee82c8bacea8546da1d3de9c72c741557b93e
yd0384 <youngdeok9@naver.com>
2021-09-30 19:59:47 +0900

    0930
    테마페이지추가

--- frontend/src/components/ThemePage.vue
+++ frontend/src/components/ThemePage.vue
@@ -1,113 +1,69 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+  <div>
+    <h1>Theme</h1>
+    <div>
+      <b-list-group horizontal>
+        <b-list-group-item v-for="branch in branchs" :key=branch.id v-on:click="select_branch(branch.id)">
+          {{ branch.name }}
+          <b-badge variant="primary" pill>{{branch.themes.length}}</b-badge>
+        </b-list-group-item>
+      </b-list-group>
+      <b-card
+        v-for="theme in branchs[selected_branch].themes" :key=theme.id
+        :title="theme.name"
+        img-src="https://picsum.photos/600/300/?image=25"
+        img-alt="Image"
+        img-left
+        tag="article"
+        class="mb-5"
+      >
+        <b-card-text>
+          Some quick example text to build on the card title and make up the bulk of the card's content.
+        </b-card-text>
+        <b-button href="#" variant="primary">예약하기</b-button>
+      </b-card>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  data () {
+  data()  {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      selected_branch:0,
+      branchs: [
+        { 
+          id: 0,
+          name: '건대1호점' ,
+          themes : [
+            { 
+              id: 0,
+              name: '루시드드림',
+            },
+            {
+              id: 1,
+              name: '콜드케이스',
+            }
+          ]
+        },
+        { 
+          id: 1,
+          name: '건대2호점' ,
+          themes : [
+            { 
+              id: 0,
+              name: '디어마르시',
+            }
+          ]
+        }
+      ]
+    }
+  },
+  methods:{
+    select_branch(id){
+      this.selected_branch=id;
     }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
