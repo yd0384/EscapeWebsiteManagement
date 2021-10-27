7afcf2a5c4da561372067a30e77e7c181fecce58
skyfjinjong <56257923+skyfjinjong@users.noreply.github.com>
2021-10-28 00:32:23 +0900

    1028
    지점

--- frontend/src/components/ThemePage.vue
+++ frontend/src/components/ThemePage.vue
@@ -3,12 +3,12 @@
     <h1 style="margin-bottom:-5px;">Theme</h1>
     [테마정보]
     <div>
-      <b-list-group horizontal>
+      <b-list-group horizontal style="display:flex; justify-content:center; margin:50px;">
         <b-list-group-item :class="{'active' : isSelected(branch.id) }" v-for="branch in branches" :key=branch.id v-on:click="select_branch(branch.id)">
           {{ branch.name }}
         </b-list-group-item>
       </b-list-group>
-      <div v-if="theme_view" style="margin-top:50px;">
+      <div v-if="theme_view" style="margin-top:100px;">
         <b-card
           v-for="theme in theme_view" :key=theme.id
           :title="theme.title"
@@ -19,32 +19,32 @@
           tag="article"
           class="mb-5"
           align="left"
-          style="width:1000px; margin:auto;"
+          style="width:1100px; margin:auto;"
           >
-          <b-card-text>
-            {{ theme.content }}
-          </b-card-text>
           <b-card-text>
             장르 : &nbsp; {{ theme.genres }}
           </b-card-text>
-          <b-card-text>
+          <b-card-text style="margin-top:-10px;">
             문제구성 : &nbsp; 장치 {{ theme.device_importance }}%, 자물쇠 {{ get_lock_importance(theme.device_importance) }}%
           </b-card-text>
-          <b-card-text>
+          <b-card-text style="margin-top:-10px;">
             플레이시간 : &nbsp; {{ theme.length }}분
           </b-card-text>
-          <b-card-text>
+          <b-card-text style="margin-top:-10px;">
             <b-row>
               <b-col cols="2">
                 난이도 : 
               </b-col>
-              <b-col style="margin-left:-50px; margin-bottom:-20px;">
+              <b-col style="margin-left:-65px; margin-bottom:-20px;">
                 <b-form-rating v-model = "theme.difficulty" readonly no-border variant="warning" style="width:100px; margin-top:-9px;"></b-form-rating>
               </b-col>
             </b-row>
           </b-card-text>
-          <b-card-text>
+          <b-card-text style="margin-top:-10px;">
             권장인원 : &nbsp; {{ theme.recommended_number }}
+          </b-card-text>
+          <b-card-text style="margin-top:30px; margin-bottom:30px; font-size:18px;">
+            <strong>{{ theme.content }}</strong>
           </b-card-text>
           <router-link :to="{ name: 'ReservationPage', params: { branch_id: selected_branch }}"> <b-button variant="primary">예약하기</b-button></router-link>
         </b-card>
@@ -87,5 +87,6 @@ export default {
 .card_title{
   font-size: 100px;
   align-content: center;
+  color:rgb(33, 22, 83)
 }
 </style>
