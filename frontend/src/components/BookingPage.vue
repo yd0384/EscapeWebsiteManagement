66d68d170de4d909a75ab0c7dcd18effd861b16d
yd0384 <youngdeok9@naver.com>
2021-10-27 23:41:36 +0900

    1027
    예약페이지조정

--- frontend/src/components/BookingPage.vue
+++ frontend/src/components/BookingPage.vue
@@ -7,67 +7,130 @@
     <hr class="mx-5">
     <h4 style="margin-bottom:50px;">예약정보</h4>
     <b-container style="width:35%">
-      <b-row align-h="center">
-        <b-col align-self="start"><b>지점명</b></b-col>
-        <b-col align-self="center">{{ branch_name }}</b-col>
-        <b-col align-self="end"></b-col>
-      </b-row>
-      <hr style="width:100%; margin-top:5px;">
-      <b-row align-h="center">
-        <b-col align-self="start"><b>예약일</b></b-col>
-        <b-col align-self="center">{{ Play_Date.getFullYear() }}년 {{ Play_Date.getMonth()+1 }}월 {{ Play_Date.getDate() }}일</b-col>
-        <b-col align-self="end"></b-col>
-      </b-row>
-      <hr style="width:100%; margin-top:5px;">
-      <b-row align-h="center">
-        <b-col align-self="start"><b>예약시간</b></b-col>
-        <b-col align-self="center">{{ items[2] }}</b-col>
-        <b-col align-self="end"></b-col>
-      </b-row>
-      <hr style="width:100%; margin-top:5px;">
-      <b-row align-h="center">
-        <b-col align-self="start"><b>테마명</b></b-col>
-        <b-col align-self="center">{{ theme_name }}</b-col>
-        <b-col align-self="end"></b-col>
-      </b-row>
-      <hr style="width:100%; margin-top:5px;">
-      <b-row align-h="center">
-        <b-col align-self="start"><b>예약자명</b></b-col>
-        <b-col align-self="center"><b-input type="text" style="height:25px; font-size : 0.9rem;" class="input" v-model="booker_name"></b-input></b-col>
-        <b-col align-self="end"></b-col>
-      </b-row>
-      <hr style="width:100%; margin-top:5px;">
-      <b-row align-h="center">
-        <b-col align-self="start"><b>전화번호</b></b-col>
-        <b-col align-self="center">
-          <div>
-            <b-row align-v="center" cols="3">
-              <b-col><b-form-select v-model="firstphone" :options="options" value-field="item" text-field="name" style="margin-bottom: 0.5px; width:60px; height:25px; font-size : 0.7rem;"></b-form-select>
-              </b-col>
-              <b-col><b-input type="text" style="width:60px; height:25px; font-size : 0.9rem;" class="input" v-model="secondphone"></b-input></b-col>
-              <b-col><b-input type="text" style="width:60px; height:25px; font-size : 0.9rem;" class="input" v-model="thirdphone"></b-input></b-col>
-            </b-row>
-          </div>
-        </b-col>
-        <b-col align-self="end"></b-col>
-      </b-row>
-      <hr style="width:100%; margin-top:5px;">
-      <b-row align-h="center">
-        <b-col align-self="start"><b>인원</b></b-col>
-        <b-col align-self="center"><b-form-select v-model="idx" :options="headcounts" value-field="idx" text-field="name" style="margin-bottom: 0.5px; width:60px; height:25px; font-size : 0.65rem;"></b-form-select></b-col>
-        <b-col align-self="end"></b-col>
-      </b-row>
-      <hr style="width:100%; margin-top:5px;">
-      <b-row align-h="center">
-        <b-col align-self="start"><b>이용가격</b></b-col>
-        <b-col align-self="center" style="color:blue;"><strong>{{ cost[idx] }}</strong></b-col>
-        <b-col align-self="end"></b-col>
-      </b-row>
-      <hr style="width:100%; margin-top:5px;">
-      <b-row>
-        <b-col align-self="start"><b-button @click="postBooking" variant="primary">예약하기 </b-button></b-col>
-        <b-col align-self="end"><b-button @click="redirectToReservation">돌아가기 </b-button></b-col>
-      </b-row>
+      <b-form @submit="onSubmit" @reset="onReset">
+        <b-form-group
+          id="input-group-1"
+          label="지점명: "
+          label-for="input-1"
+          label-cols="auto"
+        >
+          <b-form-input
+            id="input-1"
+            v-model="branch_name"
+            disabled
+            style="background-color: #FFFFFF; text-align: center"
+          ></b-form-input>
+          <hr style="width:100%; margin-top:5px;">
+        </b-form-group>
+        <b-form-group
+          id="input-group-2"
+          label="예약일: "
+          label-for="input-2"
+          label-cols="auto"
+        >
+          <b-form-input
+            id="input-2"
+            v-model="play_date"
+            disabled
+            style="background-color: #FFFFFF; text-align: center"
+          ></b-form-input>
+          <hr style="width:100%; margin-top:5px;">
+        </b-form-group>
+        <b-form-group
+          id="input-group-3"
+          label="예약시간: "
+          label-for="input-3"
+          label-cols="auto"
+        >
+          <b-form-input
+            id="input-3"
+            v-model="items[2]"
+            disabled
+            style="background-color: #FFFFFF; text-align: center"
+          ></b-form-input>
+          <hr style="width:100%; margin-top:5px;">
+        </b-form-group>
+        <b-form-group
+          id="input-group-4"
+          label="테마명: "
+          label-for="input-4"
+          label-cols="auto"
+        >
+          <b-form-input
+            id="input-4"
+            v-model="theme_name"
+            disabled
+            style="background-color: #FFFFFF; text-align: center"
+          ></b-form-input>
+          <hr style="width:100%; margin-top:5px;">
+        </b-form-group>
+        <b-form-group
+          id="input-group-5"
+          label="예약자명: "
+          label-for="input-5"
+          label-cols="auto"
+        >
+          <b-form-input
+            id="input-5"
+            v-model="booker_name"
+            required
+            style="background-color: #FFFFFF; text-align: center"
+          ></b-form-input>
+          <hr style="width:100%; margin-top:5px;">
+        </b-form-group>
+        <b-form-group
+          id="input-group-6"
+          label="전화번호: "
+          label-for="input-6"
+          label-cols="auto"
+        >
+          <b-form-select
+            id="input-6"
+            v-model="firstphone"
+            :options="options"
+            value-field="item"
+            text-field="name"
+            required
+            style="background-color: #FFFFFF; text-align: center"
+          ></b-form-select>
+          <b-form-input
+            id="input-7"
+            v-model="secondphone"
+            required
+            style="background-color: #FFFFFF; text-align: center"
+          ></b-form-input>
+          <b-form-input
+            id="input-8"
+            v-model="thirdphone"
+            required
+            style="background-color: #FFFFFF; text-align: center"
+          ></b-form-input>
+          <hr style="width:100%; margin-top:5px;">
+        </b-form-group>
+        <b-form-group
+          id="input-group-7"
+          required
+          label="인원: "
+          label-for="input-9"
+          label-cols="auto"
+        >
+          <b-form-select
+            id="input-9"
+            v-model="idx"
+            :options="headcounts"
+            value-field="idx"
+            text-field="name"
+            required
+            style="background-color: #FFFFFF; text-align: center"
+          ></b-form-select>
+          <hr style="width:100%; margin-top:5px;">
+        </b-form-group>
+        <b-form-row> 이용가격 </b-form-row>
+        <b-form-row> {{ cost[idx] }} </b-form-row>
+        <hr style="width:100%; margin-top:5px;">
+        <b-button type="submit" variant="primary"> 예약하기 </b-button>
+        <b-button type="reset"> 초기화하기 </b-button>
+      </b-form>
     </b-container>
   </div>
 </template>
@@ -95,6 +158,9 @@
         cost: 'getCost',
         headcounts: 'getHeadCounts'
       }),
+      play_date: function(){
+        return this.Play_Date.getFullYear()+'년 '+(this.Play_Date.getMonth()+1)+'월 '+this.Play_Date.getDate()+'일';
+      }
     },
     data() {
       return {
@@ -142,6 +208,41 @@
         .catch(error=>{
           console.log(error);
         })
+      },
+      onSubmit: function(event){
+        event.preventDefault(event);
+        let st = this.get_hour_and_minute(this.items[2]);
+        let en = this.get_hour_and_minute(this.items[3]);
+        let start_day = new Date(this.Play_Date);
+        let end_day = new Date(this.Play_Date);
+        let start_time=this.date_to_mysql(start_day.setHours(st[0],st[1]));
+        let end_time=this.date_to_mysql(end_day.setHours(en[0],en[1]));
+        let payload = {
+          theme_id: this.items[1],
+          start_time: start_time,
+          end_time: end_time,
+          status: 0,
+          reserved_time: this.date_to_mysql(new Date()),
+          number_of_player: this.headcounts[this.idx].item,
+          phone_number: this.firstphone+'-'+this.secondphone+'-'+this.thirdphone,
+          booker_name: this.booker_name,
+          booker_ip: this.user_ip
+        };
+        createReservation(payload)
+        .then(res => {
+          this.$router.push({name: 'ReservationCompletePage'});
+        })
+        .catch(error=>{
+          console.log(error);
+        })
+      },
+      onReset: function(event){
+        event.preventDefault();
+        this.idx=0;
+        this.secondphone='';
+        this.thirdphone='';
+        this.booker_name='';
+        this.firstphone='010';
       },
       date_to_mysql: function(d){
         let time = new Date(d)
